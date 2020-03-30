2535603e1faf81f94376dea0c316b75dcb4704cc
callisto <callisto@example.com>
2020-03-30 10:45:36 +0000

    refactor jupyter start

--- jupyter_ide/start_lab.sh
+++ jupyter_ide/start_lab.sh
@@ -8,6 +8,7 @@ echo "Enter git user name:"
 read -s gitusername
 git config --global user.email $gituseremail
 git config --global user.name $gitusername
+git config --global credential.helper store
 
 echo "JupyterLab is now running. Go to localhost:8888 and enter 'hallo'"
 conda run -n jupyter jupyter lab --ip=* --port=8888
