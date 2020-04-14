9d568150271ecd6749858270203347f7fefc9ffe
calisto <calisto>
2020-04-14 19:46:49 +0000

    worked on ground structure

--- src/settlingInSpace/game/main.py
+++ src/settlingInSpace/game/main.py
@@ -1,25 +1,34 @@
 import threading
 import time
 
-class Game():
-    heartbeat_sec = 1
-
-    def game_initialize(self):
-        # code for game initialization
-        print('initialize game')
+class SettlingMain():
+    def __init__(self):
         pass
+    
+    def start(self, servermode=False):
+        self.game_engine = GameEngine()
+        
+    def start_gameengine(self):
+        self.game_engine.start()
 
-    def game_logic(self):
-        # code to execute game logic
-        print('game logic')
-        pass
-
-    def heartbeat(self):
-        while True:
-            self.game_logic()
-            time.sleep(self.heartbeat_sec)
-
+class GameEngine():
+    def __init__(self):
+        # TODO: read it from a config yaml
+        self.heartbeat_sec = 1
+    
     def start(self):
-        self.game_initialize()
-        heartbeat_t = threading.Thread(name='heartbeat_t', target=self.heartbeat)
+        self.startHeartBeat()
+    
+    def startHeartBeat(self):
+        heartbeat_t = threading.Thread(name='heartbeat_t', target=GameEngine.heartbeat, args=(self,))
         heartbeat_t.start()
+    
+    @staticmethod
+    def heartbeat(game_engine):
+        while True:
+            time.sleep(game_engine.heartbeat_sec)
+            game_engine.beat()
+    
+    def beat(self):
+        print('play')
+        pass
