e38b0c8fd524a99e669b4e942a1831c2fc264814
calisto107 <calisto107@mail.com>
2020-04-19 20:31:49 +0000

    working technical on frontent gui in combi to jupyterlab

--- jupyter_ide/start_lab.sh
+++ jupyter_ide/start_lab.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-echo "Enter git user E-Mail:"
-read -s gituseremail
-echo "Enter git user name:"
-read -s gitusername
-git config --global user.email $gituseremail
-git config --global user.name $gitusername
+git config --global user.email $GITUSEREMAIL
+git config --global user.name $GITUSERNAME
 git config --global credential.helper store
 
 echo "JupyterLab is now running. Go to localhost:8888 and enter 'hallo'"
